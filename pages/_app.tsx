920eeb9095b6fc5b8f867ffc5bf00941defadbcb
romkery <romkery@gmail.com>
2022-11-17 17:21:19 +0700

    disable transition when page loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,29 @@
 import type { AppProps } from 'next/app';
 import Header from '@components/Header/Header';
 import '../styles/styles.scss';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const tabs = ['Home', 'Recipes', 'Ingredients', 'Drink', 'Liked'];
 
+  useEffect(() => {
+    const onPageLoad = () => {
+      document.getElementById('app')!.classList.remove('preload');
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
+
   return (
     <>
-      <div className="app">
+      <div id="app" className="app preload">
         <Header />
         <main className="main">
           <img
