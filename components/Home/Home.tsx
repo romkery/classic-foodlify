b08d7edf80054fd33bd8544226506d489bdeb001
romkery <romkery@gmail.com>
2022-11-18 20:47:21 +0700

    announcement grid layout with adaptivity was created

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,13 +4,27 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home">
-        <div className="home__hero">
+        <div className="hero">
           <img src="/logo.svg" alt="logo" />
           <h1>
             foodlify <br />
             Cook <span>delicious</span> food
           </h1>
           <p>even when there is no time</p>
+        </div>
+        <div className="announcement-grid-layout-container">
+          <div className="announcement-grid-layout">
+            <div className="announcement-grid-layout__taste">taste</div>
+            <div className="announcement-grid-layout__receipt">receipt</div>
+            <div className="announcement-grid-layout__wine">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              aspernatur atque cum eligendi eos eum, fugit magni modi neque
+              numquam quas quasi quis quisquam, ratione reiciendis rem soluta
+              tenetur. Expedita.
+            </div>
+            <div className="announcement-grid-layout__fact">fact</div>
+            <div className="announcement-grid-layout__advice">advice</div>
+          </div>
         </div>
       </div>
     </div>
